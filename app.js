4732b7217a143794632cc7ed1facf521dd114f8a
Angel Marquez <alexis.mplt@outlook.es>
2023-05-21 20:34:08 -0600

    config env global

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var cors = require("cors");
+require("dotenv").config();
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var apiV1 = require("./routes/v1");
+
+const connectionNoSql = require("./connection/nosql.conn");
+connectionNoSql();
 
 var app = express();
 app.use(cors());
