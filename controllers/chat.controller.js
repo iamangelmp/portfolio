f530cd3c91937bc2de31a5963022f7d82130542d
Angel Marquez <alexis.mplt@outlook.es>
2023-05-27 16:08:00 -0600

    create readmessage function

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,5 +1,19 @@
-const chatUser = (req, res, next) =>{
+const data = require("../connection/data");
 
+const chatUser = async (req, res, next) => {
+  const msg = req.body.msg;
+  const flow = await readMessage(msg);
+  res.json({ msg, flow });
+};
+
+function formatterMessage(text) {
+  return text.trim().toLocaleLowerCase();
 }
+
+const readMessage = async (msg) => {
+  return formatterMessage(msg);
+};
+
+const responseMessage = async () => {};
 
 module.exports = chatUser;
